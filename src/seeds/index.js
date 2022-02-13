c543ac7802f0aa1205716e109823cecae1ebdaa7
Yashemabeth <yashemabethmcleod@gmail.com>
2022-02-13 15:29:24 +0000

    Updated index.js

--- src/seeds/index.js
+++ src/seeds/index.js
@@ -4,24 +4,58 @@ const thoughts = require("./thoughts");
 
 const mongoose = require("mongoose");
 
-const {User, Thought}= require("../models");
+const { User, Thought } = require("../models");
 
 const init = async () => {
-    await mongoose.connect("mongodb://localhost:27017/socialDB", {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    })
-    console.log("database connection successful")
-    await User.deleteMany({});
-    await User.insertMany(users);
+  await mongoose.connect("mongodb://localhost:27017/socialDB", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+  console.log("database connection successful");
+  await User.deleteMany({});
 
-    console.log("[INFO]: Successfully seeded users");
+  const usersFromDB = await User.insertMany(users);
+  
 
-    await Thought.deleteMany({});
-    await Thought.insertMany(thoughts);
+  console.log("[INFO]: Successfully seeded users");
 
-    console.log("[INFO]: Successfully seeded thoughts");
+  await Thought.deleteMany({});
+  const thoughtsFromDb = await Thought.insertMany(thoughts);
+
+  console.log("[INFO]: Successfully seeded thoughts");
+
+  console.log(thoughtsFromDb)
+  // seed thoughts for users
+  const allthoughts = thoughtsFromDb.map(async (thought) => {
+    const username = thought.username;
+
+    const user = usersFromDB.find((user) => user.username === username);
+
+    user.thoughts.push(thought._id.toString());
+
+    console.log(user)
+
+    await User.findByIdAndUpdate(user._id, { ...user });
+  });
+  await Promise.all(allthoughts);
+
+  const friendsPromise = usersFromDB.map(async (user) => {
+    const userName = user.username;
+    const allUsers = usersFromDB.filter(
+      (currentUser) => currentUser.username != userName
+    );
+
+    const randomFriend =
+      usersFromDB[Math.floor(Math.random() * allUsers.length)];
+
+    user.friends.push(randomFriend._id);
+
+    await User.findByIdAndUpdate(user._id, { ...user });
+  });
+
+  await Promise.all(friendsPromise);
+
+  await mongoose.disconnect();
 };
 
-init ();
-
+init();
